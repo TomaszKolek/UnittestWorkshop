ddd0156db865414035ee68587ba4d7366ad8d8cf
kolek <tomasz.kolek@nsn.com>
2015-03-12 11:24:28 +0100

    add part1 solution

--- workshop/basic_assertions/workshop_tests_to_fill.py
+++ workshop/basic_assertions/workshop_tests_to_fill.py
@@ -3,35 +3,34 @@ import unittest
 from code_to_test import MyFantasticClass, Person
 
 
-class TestMyFantasticClass(ChangeMe):
-    # WHAT CLASS SHOULD TEST INHERIT FROM?
+class TestMyFantasticClass(unittest.TestCase):
 
-    def change_me(self):
-        # WHAT IS NAME OF METHOD EXECUTED BEFORE EVERY TEST?
-        # WHY WE SHOULD USE IT INSTEAD OF METHOD EXECUTED ONCE BEFORE ALL TESTS?
-        self.my_fantastic_object = MyFantasticClass(index=0)
+    def setUp(self):
+        # WE NEED TO CREATE "FRESH" OBJECT BEFORE EVERY TEST TO BE SURE THAT WE ARE OPERATING ON NOT MODIFIED OBJECT
+        self.index = 0
+        self.my_fantastic_object = MyFantasticClass(index=self.index)
 
-    def check_if_index_was_correctly_set(self):
-        # IS EVERYTHING OK WITH TEST METHOD NAME?
-        # ADD TESTS HERE -> CHECK IF 0 WAS SET AS INDEX
-        pass
+    def test_check_if_index_was_correctly_set(self):
+        self.assertEqual(self.my_fantastic_object.index, 0)
 
     def test_check_if_correct_name_will_be_returned_by_get_name_by_index_method(self):
-        # ADD TESTS HERE -> CHECK IF RETURNED NAME WILL BE CORRECT ELEMENT FROM names_list
-        pass
+        self.assertEqual(self.my_fantastic_object.get_name_by_index(), self.my_fantastic_object.names_list[self.index])
 
     def test_check_if_get_name_by_index_method_raises_error_when_index_is_not_correct(self):
         index = 3
-        # ADD TESTS HERE -> CHECK IF METHOD RAISES INDEX ERROR
+        self.my_fantastic_object.index = index
+        self.assertRaises(IndexError, self.my_fantastic_object.get_name_by_index)
+        with self.assertRaises(IndexError):
+            self.my_fantastic_object.get_name_by_index()
 
     def test_check_if_get_random_name_returns_correct_name(self):
         random_name = self.my_fantastic_object.get_random_name()
-        # ADD TESTS HERE -> CHECK IF RETURNED NAME IS ONE FROM THE LIST
+        self.assertIn(random_name, self.my_fantastic_object.names_list)
 
     def test_check_if_get_person_returns_person_class_object(self):
         potential_person_object = self.my_fantastic_object.get_person()
-        # ADD TESTS HERE -> CHECK IF RETURNED OBJECT IS PERSON CLASS OBJECT
-        # LOOK AT THE CODE -> WHY THIS TEST IS NOT GOOD ONE?
+        self.assertIsInstance(potential_person_object, Person)
+        # NO WE SHOULD TEST SEPARATED FUNCTIONALITY HERE WE EXECUTE TESTING METHOD AND GET_NAME_BY_INDEX
 
 
 if __name__ == "__main__":
